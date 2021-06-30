60d4b5dab97f0724af837240aed899c9dce8cd63
Ala BUVEIDANI <86134227+Alaa-bu@users.noreply.github.com>
2021-06-30 13:40:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var s = document.getElementById("btn");
+s.addEventListener("click", 'newItem');
+
+function newItem1() {
+    window.location.assign("https://www.linkedin.com/in/alaa-buve%C4%B1dan%C4%B1-530673212");
+}
+function newItem2() {
+    window.location.assign("https://github.com/Alaa-bu");
+}
+
+function myFunction() {
+    document.getElementById("nameSurname").innerHTML = "ALA BUVEIDANI";
+    document.getElementById("age").innerHTML = "23";
+}
